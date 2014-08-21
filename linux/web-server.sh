0b3b99bb25a88d4fc2e167eaf07e9a42fd2b8e65
Vlad <dev@vld.me>
2014-08-21 05:57:51 +0400

    Update web-server.sh

--- linux/web-server.sh
+++ linux/web-server.sh
@@ -1,40 +1,46 @@
-#!/bin/bash
- 
+#!/bin/sh
+
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
- 
-apt-get install -y python-software-properties
- 
+
+apt-get install -y software-properties-common python-software-properties
+
+apt-get install -y curl
+
+
 add-apt-repository -y ppa:ondrej/php5
+add-apt-repository -y ppa:nginx/stable
+add-apt-repository -y ppa:git-core/ppa
 apt-get update
+
+# php
 apt-get install -y php5-cli php5-fpm php5-mysql php5-apcu php5-intl php5-gd php5-curl
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
- 
-add-apt-repository -y ppa:ondrej/mysql-5.6
-apt-get update
+
 apt-get install -y mysql-server
- 
-add-apt-repository -y ppa:nginx/stable
-apt-get update
+apt-get install -y mysql-client
+
 apt-get install -y nginx-full
- 
-add-apt-repository -y ppa:git-core/ppa
-apt-get update
 apt-get install -y git-core
- 
-apt-get install -y ruby1.9.1
-gem install rmate
-gem install sass
-gem install compass
 
+# ruby
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+source ~/.rvm/scripts/rvm
+gem install rmate sass compass
 
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
+# nodejs
 apt-get install -y nodejs
-npm install -g gulp bower coffee-script
- 
+apt-get install -y npm
+npm install -g grunt gulp bower coffee-script
+
+echo "Enter username"
+read username
+
+adduser $username
+adduser $username sudo
+
 dpkg-reconfigure tzdata
- 
+
 reboot
